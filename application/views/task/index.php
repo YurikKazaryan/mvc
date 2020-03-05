c10c20b0658a65f23e61a29d92415d6f84b811ec
Yurik Kazaryan <yurik.kazaryan@ya.ru>
2020-03-05 17:18:40 +0900

    task list in table done

--- application/views/task/index.php
+++ application/views/task/index.php
@@ -1,6 +1,20 @@
-<table class="table">
+<table class="table table-striped table-hover" style="margin-top: 15px">
   <thead class="thead-dark">
     <tr>
-      <th scope="col"></th>
+      <th>Имя пользователя</th>
+      <th>Email</th>
+      <th>Текст задачи</th>
+      <th>Статус</th>
+    </tr>
+  </thead>
+  <tbody>
     <?php foreach ($model as $task): ?>
+    <tr>
+      <td><?=$task['username']?></td>
+      <td><?=$task['email']?></td>
+      <td><?=$task['task_text']?></td>
+      <td><?=$task['status'] == 1 ? 'Активная' : 'Закрытая'?></td>
+    </tr>
     <?php endforeach?>
+  <tbody>
+</table>
